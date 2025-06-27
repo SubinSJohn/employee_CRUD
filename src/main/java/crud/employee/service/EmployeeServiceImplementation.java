aaebb247ed58a6843757bd6294cdce31cd79b5b3
subin <subinsjohn28@gmail.com>
2025-06-27 11:58:21 +0530

    employee crud app first commit

---
+++ src/main/java/crud/employee/service/EmployeeServiceImplementation.java
@@ -0,0 +1,22 @@
+package crud.employee.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import crud.employee.entity.Employee;
+import crud.employee.repository.EmployeeRepository;
+
+
+@Service
+public class EmployeeServiceImplementation implements EmployeeService {
+	
+	@Autowired
+	EmployeeRepository repo;
+
+	@Override
+	public void createNewEmployee(Employee emp) {
+		repo.save(emp);
+
+	}
+
+}
